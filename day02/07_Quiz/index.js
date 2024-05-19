ccb6ea07f39fd932a65c846d2e291c89b13849ac
LSG <166900725+webLSG@users.noreply.github.com>
2024-05-19 23:19:36 +0900

    homework

--- day02/07_Quiz/index.js
+++ day02/07_Quiz/index.js
@@ -1,26 +1,34 @@
+// 총액
 totalCnt = document.querySelector(".totalCnt");
 totalPrice = document.querySelector(".totalPrice");
 
+// 아메리카노
 americanoBtn = document.querySelector(".americanoBtn");
-latteBtn = document.querySelector(".latteBtn");
-vanilaBtn = document.querySelector(".vanilaBtn");
 
 americanoBtn.addEventListener("click", () => {
   totalCnt.innerText = Number(totalCnt.innerText) + 1;
   totalPrice.innerText = Number(totalPrice.innerText) + 3000;
 });
 
+// 라떼
+latteBtn = document.querySelector(".latteBtn");
+
 latteBtn.addEventListener("click", () => {
   totalCnt.innerText = Number(totalCnt.innerText) + 1;
   totalPrice.innerText = Number(totalPrice.innerText) + 3500;
 });
+
+// 바닐라라떼
+vanilaBtn = document.querySelector(".vanilaBtn");
 
 vanilaBtn.addEventListener("click", () => {
   totalCnt.innerText = Number(totalCnt.innerText) + 1;
   totalPrice.innerText = Number(totalPrice.innerText) + 4000;
 });
 
+// reset
 resetBtn = document.querySelector(".resetBtn");
+
 resetBtn.addEventListener("click", () => {
   totalCnt.innerText = 0;
   totalPrice.innerText = 0;
