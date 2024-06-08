8f6a74f1f6009d55c6cfcdbd07c8edc15c345165
LSG <166900725+webLSG@users.noreply.github.com>
2024-06-08 23:53:23 +0900

    diff

--- day07/05_weather/index.js
+++ day07/05_weather/index.js
@@ -1,86 +1,48 @@
-const APIkeys = "5bb21517a62ec8ea076e4e76cb0629b2";
-const weather = document.querySelector(".weather");
-
-/**
-weatherAPI
-  .then((v) => v.json())
-  .then((v) => {
-    const weather = document.querySelector(".weather");
-    weather.innerText = `부평역 날씨: ${v.weather[0].main}`;
-  });
-*/
-
-const bupyeong = document.querySelector(".bupyeong");
-bupyeong.addEventListener("click", () => {
-  const lat = 37.48945088711253;
-  const lon = 126.72434917358073;
-  const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIkeys}`;
-  const weatherAPI = fetch(URL);
-
-  weatherAPI
+// 위도(latitude) 경도(longitude)
+// 함수
+const weatherFunc = (lat, lon) => {
+  const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=5bb21517a62ec8ea076e4e76cb0629b2`;
+  const getWeatherData = fetch(URL);
+  getWeatherData
     .then((v) => v.json())
-    .then((v) => v.weather[0].main)
-    .then((v) => {
-      weather.innerText = `부평역 날씨: ${v}`;
+    .then(({ name, weather }) => {
+      const weatherBox = document.querySelector(".weatherBox");
+      weatherBox.innerHTML = "";
+      weatherBox.insertAdjacentHTML(
+        "beforeend",
+        `
+				<h1 class="weather">${name} Weather: ${weather[0].main}</h1>
+				`
+      );
     });
-});
+};
 
+// 서울
+const seoulLat = 37.566535;
+const seoulLon = 126.9779692;
+const seoul = document.querySelector(".seoul");
+seoul.addEventListener("click", () => weatherFunc(seoulLat, seoulLon));
+
+// 후쿠오카
+const fukuokaLat = 33.5903547;
+const fukuokaLon = 130.4017155;
 const fukuoka = document.querySelector(".fukuoka");
-fukuoka.addEventListener("click", () => {
-  const lat = 33.5842357;
-  const lon = 130.3882332;
-  const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIkeys}`;
-  const weatherAPI = fetch(URL);
+fukuoka.addEventListener("click", () => weatherFunc(fukuokaLat, fukuokaLon));
 
-  weatherAPI
-    .then((v) => v.json())
-    .then((v) => v.weather[0].main)
-    .then((v) => {
-      weather.innerText = `후쿠오카 날씨: ${v}`;
-    });
-});
-
+// 칭다오
+const qingdaoLat = 36.067117;
+const qingdaoLon = 120.382612;
 const qingdao = document.querySelector(".qingdao");
-qingdao.addEventListener("click", () => {
-  const lat = 36.0247;
-  const lon = 120.357;
-  const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIkeys}`;
-  const weatherAPI = fetch(URL);
+qingdao.addEventListener("click", () => weatherFunc(qingdaoLat, qingdaoLon));
 
-  weatherAPI
-    .then((v) => v.json())
-    .then((v) => v.weather[0].main)
-    .then((v) => {
-      weather.innerText = `칭다오 날씨: ${v}`;
-    });
-});
-
+// 타이페이
+const taipeiLat = 25.0671234;
+const taipeiLon = 121.611452;
 const taipei = document.querySelector(".taipei");
-taipei.addEventListener("click", () => {
-  const lat = 25.011;
-  const lon = 121.55;
-  const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIkeys}`;
-  const weatherAPI = fetch(URL);
+taipei.addEventListener("click", () => weatherFunc(taipeiLat, taipeiLon));
 
-  weatherAPI
-    .then((v) => v.json())
-    .then((v) => v.weather[0].main)
-    .then((v) => {
-      weather.innerText = `타이페이 날씨: ${v}`;
-    });
-});
-
+// 시드니
+const sydneyLat = -33.8674769;
+const sydneyLon = 151.2069776;
 const sydney = document.querySelector(".sydney");
-sydney.addEventListener("click", () => {
-  const lat = -33.8667;
-  const lon = 151.2;
-  const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIkeys}`;
-  const weatherAPI = fetch(URL);
-
-  weatherAPI
-    .then((v) => v.json())
-    .then((v) => v.weather[0].main)
-    .then((v) => {
-      weather.innerText = `시드니 날씨: ${v}`;
-    });
-});
+sydney.addEventListener("click", () => weatherFunc(sydneyLat, sydneyLon));
